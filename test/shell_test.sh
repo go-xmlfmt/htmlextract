8b5fc806b1b7bdd75b790444e01b77f885d10c8d
Antonio Sun <asun@ats.l5.ca>
2018-01-07 09:53:26 -0500

    - [+] add shell_test.sh

---
+++ test/shell_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# test first
+htmlextract outline -i sample0.html -o /tmp/htmlextract.sample0.json.ref
+echo $?
+htmlextract outline -i sample1.html -o /tmp/htmlextract.sample1.json.ref
+echo $?
+htmlextract outline -i sample2.html -o /tmp/htmlextract.sample2.json.ref
+echo $?
+
+# check next
+ls sample?.json | xargs -t -i sh -c 'diff -U1 {} /tmp/htmlextract.{}.ref'
+ret=$?
+echo $ret
+exit $ret
